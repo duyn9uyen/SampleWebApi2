5f66e68cff83c0369cc1aef9707d0338786cea7e
Duy Nguyen <duy.nguyen@alumni.pitt.edu>
2015-02-05 00:21:50 -0500

    1st Commit

---
+++ WebApi2/Controllers/SkillsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2.Controllers
+{
+    public class SkillsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Updateskill(string skillPk, string userPk)
+        {
+            return Ok("200");
+        }
+    }
+}
---
+++ WebApi2/Scripts/Home/Skill.cshtml.js
@@ -0,0 +1,27 @@
+﻿var SkillView = {
+
+    Get: function () {
+        var skillPk = "skillPk";
+        var userPk = "userPk";
+        var ApplicationRoot = window.location.origin;
+
+        $.ajax({
+            async: true,
+            type: 'GET',
+            url: ApplicationRoot + '/api/Skills/Updateskill',
+            data: { skillPk: 'skillPkValue', userPk: 'userPkValue' },
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                alert(response);
+            },
+            error: function (xhr) {
+                alert("Oops! It appears there has been an error. Please try     again.\n\nError Thrown: " + xhr.responseText);
+            }
+        });
+    }
+
+}
+
+$(function () {
+    SkillView.Get();
+});
